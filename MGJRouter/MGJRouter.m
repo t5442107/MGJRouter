7174be039b9f4277ba1e7871e9a1f4950019799b
yangji <t5442107@qq.com>
2024-10-26 13:37:26 +0800

    Update MGJRouter.m

--- MGJRouter/MGJRouter.m
+++ MGJRouter/MGJRouter.m
@@ -10,10 +10,12 @@
 #import <objc/runtime.h>
 
 static NSString * const MGJ_ROUTER_WILDCARD_CHARACTER = @"~";
+static NSString *specialCharacters = @"/?&.";
 
 NSString *const MGJRouterParameterURL = @"MGJRouterParameterURL";
 NSString *const MGJRouterParameterCompletion = @"MGJRouterParameterCompletion";
 NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
+
 
 @interface MGJRouter ()
 /**
@@ -25,7 +27,7 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
 
 @implementation MGJRouter
 
-+ (instancetype)sharedIsntance
++ (instancetype)sharedInstance
 {
     static MGJRouter *instance = nil;
     static dispatch_once_t onceToken;
@@ -37,7 +39,12 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
 
 + (void)registerURLPattern:(NSString *)URLPattern toHandler:(MGJRouterHandler)handler
 {
-    [[self sharedIsntance] addURLPattern:URLPattern andHandler:handler];
+    [[self sharedInstance] addURLPattern:URLPattern andHandler:handler];
+}
+
++ (void)deregisterURLPattern:(NSString *)URLPattern
+{
+    [[self sharedInstance] removeURLPattern:URLPattern];
 }
 
 + (void)openURL:(NSString *)URL
@@ -45,15 +52,15 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
     [self openURL:URL completion:nil];
 }
 
-+ (void)openURL:(NSString *)URL completion:(void (^)(void))completion
++ (void)openURL:(NSString *)URL completion:(void (^)(id result))completion
 {
     [self openURL:URL withUserInfo:nil completion:completion];
 }
 
-+ (void)openURL:(NSString *)URL withUserInfo:(NSDictionary *)userInfo completion:(void (^)(void))completion
++ (void)openURL:(NSString *)URL withUserInfo:(NSDictionary *)userInfo completion:(void (^)(id result))completion
 {
     URL = [URL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSMutableDictionary *parameters = [[self sharedIsntance] extractParametersFromURL:URL];
+    NSMutableDictionary *parameters = [[self sharedInstance] extractParametersFromURL:URL matchExactly:NO];
     
     [parameters enumerateKeysAndObjectsUsingBlock:^(id key, NSString *obj, BOOL *stop) {
         if ([obj isKindOfClass:[NSString class]]) {
@@ -78,36 +85,113 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
 
 + (BOOL)canOpenURL:(NSString *)URL
 {
-    return [[self sharedIsntance] extractParametersFromURL:URL] ? YES : NO;
+    return [[self sharedInstance] extractParametersFromURL:URL matchExactly:NO] ? YES : NO;
+}
+
++ (BOOL)canOpenURL:(NSString *)URL matchExactly:(BOOL)exactly {
+    return [[self sharedInstance] extractParametersFromURL:URL matchExactly:YES] ? YES : NO;
 }
 
 + (NSString *)generateURLWithPattern:(NSString *)pattern parameters:(NSArray *)parameters
 {
     NSInteger startIndexOfColon = 0;
-    NSMutableArray *items = [[NSMutableArray alloc] init];
-    NSInteger parameterIndex = 0;
+    
+    NSMutableArray *placeholders = [NSMutableArray array];
     
     for (int i = 0; i < pattern.length; i++) {
         NSString *character = [NSString stringWithFormat:@"%c", [pattern characterAtIndex:i]];
         if ([character isEqualToString:@":"]) {
             startIndexOfColon = i;
         }
-        if (([@[@"/", @"?", @"&"] containsObject:character] || (i == pattern.length - 1 && startIndexOfColon) ) && startIndexOfColon) {
-            if (i > (startIndexOfColon + 1)) {
-                [items addObject:[NSString stringWithFormat:@"%@%@", [pattern substringWithRange:NSMakeRange(0, startIndexOfColon)], parameters[parameterIndex++]]];
-                pattern = [pattern substringFromIndex:i];
-                i = 0;
+        if ([specialCharacters rangeOfString:character].location != NSNotFound && i > (startIndexOfColon + 1) && startIndexOfColon) {
+            NSRange range = NSMakeRange(startIndexOfColon, i - startIndexOfColon);
+            NSString *placeholder = [pattern substringWithRange:range];
+            if (![self checkIfContainsSpecialCharacter:placeholder]) {
+                [placeholders addObject:placeholder];
+                startIndexOfColon = 0;
             }
-            startIndexOfColon = 0;
+        }
+        if (i == pattern.length - 1 && startIndexOfColon) {
+            NSRange range = NSMakeRange(startIndexOfColon, i - startIndexOfColon + 1);
+            NSString *placeholder = [pattern substringWithRange:range];
+            if (![self checkIfContainsSpecialCharacter:placeholder]) {
+                [placeholders addObject:placeholder];
+            }
         }
     }
     
-    return [items componentsJoinedByString:@""];
+    __block NSString *parsedResult = pattern;
+    
+    [placeholders enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        idx = parameters.count > idx ? idx : parameters.count - 1;
+        parsedResult = [parsedResult stringByReplacingOccurrencesOfString:obj withString:parameters[idx]];
+    }];
+    
+    return parsedResult;
+}
+
++ (id)objectForURL:(NSString *)URL withUserInfo:(NSDictionary *)userInfo
+{
+    MGJRouter *router = [MGJRouter sharedInstance];
+    
+    URL = [URL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSMutableDictionary *parameters = [router extractParametersFromURL:URL matchExactly:NO];
+    MGJRouterObjectHandler handler = parameters[@"block"];
+    
+    if (handler) {
+        if (userInfo) {
+            parameters[MGJRouterParameterUserInfo] = userInfo;
+        }
+        [parameters removeObjectForKey:@"block"];
+        return handler(parameters);
+    }
+    return nil;
+}
+
++ (id)objectForURL:(NSString *)URL
+{
+    return [self objectForURL:URL withUserInfo:nil];
+}
+
++ (void)registerURLPattern:(NSString *)URLPattern toObjectHandler:(MGJRouterObjectHandler)handler
+{
+    [[self sharedInstance] addURLPattern:URLPattern andObjectHandler:handler];
+}
+
+- (void)addURLPattern:(NSString *)URLPattern andHandler:(MGJRouterHandler)handler
+{
+    NSMutableDictionary *subRoutes = [self addURLPattern:URLPattern];
+    if (handler && subRoutes) {
+        subRoutes[@"_"] = [handler copy];
+    }
+}
+
+- (void)addURLPattern:(NSString *)URLPattern andObjectHandler:(MGJRouterObjectHandler)handler
+{
+    NSMutableDictionary *subRoutes = [self addURLPattern:URLPattern];
+    if (handler && subRoutes) {
+        subRoutes[@"_"] = [handler copy];
+    }
+}
+
+- (NSMutableDictionary *)addURLPattern:(NSString *)URLPattern
+{
+    NSArray *pathComponents = [self pathComponentsFromURL:URLPattern];
+
+    NSMutableDictionary* subRoutes = self.routes;
+    
+    for (NSString* pathComponent in pathComponents) {
+        if (![subRoutes objectForKey:pathComponent]) {
+            subRoutes[pathComponent] = [[NSMutableDictionary alloc] init];
+        }
+        subRoutes = subRoutes[pathComponent];
+    }
+    return subRoutes;
 }
 
 #pragma mark - Utils
 
-- (NSMutableDictionary *)extractParametersFromURL:(NSString *)url
+- (NSMutableDictionary *)extractParametersFromURL:(NSString *)url matchExactly:(BOOL)exactly
 {
     NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
     
@@ -116,9 +200,9 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
     NSMutableDictionary* subRoutes = self.routes;
     NSArray* pathComponents = [self pathComponentsFromURL:url];
     
+    BOOL found = NO;
     // borrowed from HHRouter(https://github.com/Huohua/HHRouter)
     for (NSString* pathComponent in pathComponents) {
-        BOOL found = NO;
         
         // 对 key 进行排序，这样可以把 ~ 放到最后
         NSArray *subRoutesKeys =[subRoutes.allKeys sortedArrayUsingComparator:^NSComparisonResult(NSString *obj1, NSString *obj2) {
@@ -133,10 +217,26 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
             } else if ([key hasPrefix:@":"]) {
                 found = YES;
                 subRoutes = subRoutes[key];
-                parameters[[key substringFromIndex:1]] = pathComponent;
+                NSString *newKey = [key substringFromIndex:1];
+                NSString *newPathComponent = pathComponent;
+                // 再做一下特殊处理，比如 :id.html -> :id
+                if ([self.class checkIfContainsSpecialCharacter:key]) {
+                    NSCharacterSet *specialCharacterSet = [NSCharacterSet characterSetWithCharactersInString:specialCharacters];
+                    NSRange range = [key rangeOfCharacterFromSet:specialCharacterSet];
+                    if (range.location != NSNotFound) {
+                        // 把 pathComponent 后面的部分也去掉
+                        newKey = [newKey substringToIndex:range.location - 1];
+                        NSString *suffixToStrip = [key substringFromIndex:range.location];
+                        newPathComponent = [newPathComponent stringByReplacingOccurrencesOfString:suffixToStrip withString:@""];
+                    }
+                }
+                parameters[newKey] = newPathComponent;
                 break;
+            } else if (exactly) {
+                found = NO;
             }
         }
+        
         // 如果没有找到该 pathComponent 对应的 handler，则以上一层的 handler 作为 fallback
         if (!found && !subRoutes[@"_"]) {
             return nil;
@@ -144,20 +244,12 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
     }
     
     // Extract Params From Query.
-    NSArray* pathInfo = [url componentsSeparatedByString:@"?"];
-    if (pathInfo.count > 1) {
-        NSString* parametersString = [pathInfo objectAtIndex:1];
-        NSArray* paramStringArr = [parametersString componentsSeparatedByString:@"&"];
-        for (NSString* paramString in paramStringArr) {
-            NSArray* paramArr = [paramString componentsSeparatedByString:@"="];
-            if (paramArr.count > 1) {
-                NSString* key = [paramArr objectAtIndex:0];
-                NSString* value = [paramArr objectAtIndex:1];
-                parameters[key] = value;
-            }
-        }
-    }
+    NSArray<NSURLQueryItem *> *queryItems = [[NSURLComponents alloc] initWithURL:[[NSURL alloc] initWithString:url] resolvingAgainstBaseURL:false].queryItems;
     
+    for (NSURLQueryItem *item in queryItems) {
+        parameters[item.name] = item.value;
+    }
+
     if (subRoutes[@"_"]) {
         parameters[@"block"] = [subRoutes[@"_"] copy];
     }
@@ -165,28 +257,34 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
     return parameters;
 }
 
-- (void)addURLPattern:(NSString *)URLPattern andHandler:(MGJRouterHandler)handler
+- (void)removeURLPattern:(NSString *)URLPattern
 {
-    NSArray *pathComponents = [self pathComponentsFromURL:URLPattern];
+    NSMutableArray *pathComponents = [NSMutableArray arrayWithArray:[self pathComponentsFromURL:URLPattern]];
     
-    NSInteger index = 0;
-    NSMutableDictionary* subRoutes = self.routes;
-    
-    while (index < pathComponents.count) {
-        NSString* pathComponent = pathComponents[index];
-        if (![subRoutes objectForKey:pathComponent]) {
-            subRoutes[pathComponent] = [[NSMutableDictionary alloc] init];
+    // 只删除该 pattern 的最后一级
+    if (pathComponents.count >= 1) {
+        // 假如 URLPattern 为 a/b/c, components 就是 @"a.b.c" 正好可以作为 KVC 的 key
+        NSString *components = [pathComponents componentsJoinedByString:@"."];
+        NSMutableDictionary *route = [self.routes valueForKeyPath:components];
+        
+        if (route.count >= 1) {
+            NSString *lastComponent = [pathComponents lastObject];
+            [pathComponents removeLastObject];
+            
+            // 有可能是根 key，这样就是 self.routes 了
+            route = self.routes;
+            if (pathComponents.count) {
+                NSString *componentsWithoutLast = [pathComponents componentsJoinedByString:@"."];
+                route = [self.routes valueForKeyPath:componentsWithoutLast];
+            }
+            [route removeObjectForKey:lastComponent];
         }
-        subRoutes = subRoutes[pathComponent];
-        index++;
-    }
-    if (handler) {
-        subRoutes[@"_"] = [handler copy];
     }
 }
 
 - (NSArray*)pathComponentsFromURL:(NSString*)URL
 {
+
     NSMutableArray *pathComponents = [NSMutableArray array];
     if ([URL rangeOfString:@"://"].location != NSNotFound) {
         NSArray *pathSegments = [URL componentsSeparatedByString:@"://"];
@@ -194,13 +292,12 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
         [pathComponents addObject:pathSegments[0]];
         
         // 如果只有协议，那么放一个占位符
-        if ((pathSegments.count == 2 && ((NSString *)pathSegments[1]).length) || pathSegments.count < 2) {
+        URL = pathSegments.lastObject;
+        if (!URL.length) {
             [pathComponents addObject:MGJ_ROUTER_WILDCARD_CHARACTER];
         }
-        
-        URL = [URL substringFromIndex:[URL rangeOfString:@"://"].location + 3];
     }
-    
+
     for (NSString *pathComponent in [[NSURL URLWithString:URL] pathComponents]) {
         if ([pathComponent isEqualToString:@"/"]) continue;
         if ([[pathComponent substringToIndex:1] isEqualToString:@"?"]) break;
@@ -215,6 +312,13 @@ NSString *const MGJRouterParameterUserInfo = @"MGJRouterParameterUserInfo";
         _routes = [[NSMutableDictionary alloc] init];
     }
     return _routes;
+}
+
+#pragma mark - Utils
+
++ (BOOL)checkIfContainsSpecialCharacter:(NSString *)checkedString {
+    NSCharacterSet *specialCharactersSet = [NSCharacterSet characterSetWithCharactersInString:specialCharacters];
+    return [checkedString rangeOfCharacterFromSet:specialCharactersSet].location != NSNotFound;
 }
 
 @end
